d1ec7a1787503ca0f1b87ec75f268bb7d7b85f31
Jon Bergland <144770553+JonBergland@users.noreply.github.com>
2024-11-04 21:35:04 +0100

    refactor: improve Huffmancompresion with use of BitSet

--- oving6/Huffman.java
+++ oving6/Huffman.java
@@ -1,10 +1,10 @@
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.UnsupportedEncodingException;
-import java.util.ArrayList;
+import java.nio.ByteBuffer;
 import java.util.Arrays;
+import java.util.BitSet;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 import java.util.PriorityQueue;
 
@@ -17,19 +17,12 @@ public class innpakking {
 
 class Run {
     public void run() {
-        String streng = "åei ieiei eiiei ieh iehei ";
+        String streng = "åååååDetteoaoaaahhhh skiiiiittttttttttttttttt pomfrites\n"
+        + "SKjerrra Dette er en lang tekst neoeeoe bagherea +?!|";
         System.out.println(streng);
         Huffman huffman = new Huffman();
 
-        byte[] bytes = huffman.encodeStringToBytes(streng);
-
-        int[] frekvensListe = huffman.generateFrequencyList(bytes);
-
-        HuffmanNode rootNode = huffman.generateHuffmanTree(frekvensListe);
-
-        Map<Byte, List<Boolean>> codeMap = huffman.generateCodes(rootNode);
-
-        byte[] pakketInn = huffman.packData(codeMap, streng.getBytes(), frekvensListe);
+        byte[] pakketInn = huffman.packData(huffman.encodeStringToBytes(streng));
 
         System.out.println("Lengde på komprimert fil: " + pakketInn.length);
 
@@ -39,119 +32,77 @@ class Run {
 
         System.out.println(result);
 
+        System.out.println(streng.equals(result));
+
     }
 }
 
 class Huffman {
     private final int FREQUENCYLENGTH = 256;
-    
-        public Huffman() {
+
+    public Huffman() {
+    }
+
+    public byte[] packData(byte[] input) {
+        int[] frekvensListe = generateFrequencyList(input);
+
+        HuffmanNode rootNode = generateHuffmanTree(frekvensListe);
+
+        Map<Byte, BitSetCollection> codeMap = generateCodes(rootNode);
+
+        BitSet innpakketKode = new BitSet();
+
+        // Konverterer Frekvenstabellen til bytes
+        byte[] frekvensTabell = new byte[this.FREQUENCYLENGTH];
+        for (int i = 0; i < frekvensListe.length; i++) {
+            frekvensTabell[i] = (byte) frekvensListe[i];
         }
-    
-        public HuffmanNode generateHuffmanTree(int[] frekvensListe) {
-            PriorityQueue<HuffmanNode> nQueue = new PriorityQueue<>();
-    
-            for (int i = 0; i < frekvensListe.length; i++) {
-                int element = frekvensListe[i];
-                if (element > 0) {
-                    nQueue.add(new HuffmanNode((byte) i,element));
-                }
+
+        int bitIndex = 0;
+
+        for (byte b : input) {
+            BitSetCollection bitSet = codeMap.get(b);
+            for (int i = 0; i < bitSet.bitLength; i++) {
+                innpakketKode.set(bitIndex, bitSet.bitSet.get(i));
+                bitIndex++;
             }
-            
-           return generateRootNodeFromMinHeap(nQueue);
         }
-    
-        public HuffmanNode generateRootNodeFromMinHeap(PriorityQueue<HuffmanNode> nQueue) {
-            while (nQueue.size() > 1) {
-                HuffmanNode a = nQueue.poll();
-                HuffmanNode b = nQueue.poll();
-    
-                HuffmanNode combo = new HuffmanNode(a, b);
-                nQueue.add(combo);
-            }
-            return nQueue.poll();
-        }
-    
-        public Map<Byte, List<Boolean>> generateCodes(HuffmanNode rootNode) {
-            if (rootNode == null || rootNode.getClass() != HuffmanNode.class) {
-                throw new IllegalArgumentException(rootNode + " er enten null eller ikke en HuffmanNode");
-            }
-            Map<Byte, List<Boolean>> codeMap = new HashMap<>();
-    
-            exploreHuffmanNode(rootNode, new ArrayList<>(), codeMap);
-    
-            return codeMap;
-        }
-    
-        private void exploreHuffmanNode(HuffmanNode node, List<Boolean> bits, Map<Byte, List<Boolean>> codeMap) {
-            if (node.vNode == null & node.hNode == null) {
-                codeMap.put(node.tegn, new ArrayList<>(bits));
-                return;
-            }
-    
-            bits.add(false);
-            exploreHuffmanNode(node.vNode, bits, codeMap);
-            bits.remove(bits.size() -1);
-    
-            bits.add(true);
-            exploreHuffmanNode(node.hNode, bits, codeMap);
-            bits.remove(bits.size() -1);
-        }
-    
-        public byte[] packData(Map<Byte, List<Boolean>> codeMap, byte[] input, int[] frekvensListe) {
-            List<Boolean> innpakketKode = new ArrayList<>();
-    
-            // Konverterer Frekvenstabellen til bytes
-            byte[] frekvensTabell = new byte[this.FREQUENCYLENGTH];
-            for (int i = 0; i < frekvensListe.length; i++) {
-                frekvensTabell[i] = (byte) frekvensListe[i];
-            }
-    
-            for (byte b : input) {
-                innpakketKode.addAll(codeMap.get(b));
-            }
-    
-            byte[] message = convertBitsToBytes(innpakketKode);
-    
-            byte[] output = new byte[this.FREQUENCYLENGTH + message.length];
-            System.arraycopy(frekvensTabell, 0, output, 0, this.FREQUENCYLENGTH);
-            System.arraycopy(message, 0, output, this.FREQUENCYLENGTH, message.length);
-    
-    
-            return output;
-        }
-    
-        public byte[] unpackData(byte[] input) {
-        // Hent ut frekvens tabellen
+
+        BitSetCollection bitSetCollection = new BitSetCollection(innpakketKode, bitIndex);
+
+        byte[] message = convertBitsToBytes(bitSetCollection);
+
+        byte[] output = new byte[this.FREQUENCYLENGTH + message.length];
+        System.arraycopy(frekvensTabell, 0, output, 0, this.FREQUENCYLENGTH);
+        System.arraycopy(message, 0, output, this.FREQUENCYLENGTH, message.length);
+
+        return output;
+    }
+
+    public byte[] unpackData(byte[] input) {
+        // Hent ut frekvenstabellen
         byte[] frekvensTabell = new byte[this.FREQUENCYLENGTH];
         if (input.length < frekvensTabell.length) {
             throw new IllegalArgumentException("Input er mangler frekvenstabell");
         }
         System.arraycopy(input, 0, frekvensTabell, 0, this.FREQUENCYLENGTH);
         int[] frekvensListe = new int[this.FREQUENCYLENGTH];
-        Arrays.setAll(frekvensListe, i -> 0);
 
-        for (int i = 0; i < this.FREQUENCYLENGTH; i ++) {
-            if (frekvensTabell[i] > 0) {
-                frekvensListe[i] = (int) input[i];
-            }
+        for (int i = 0; i < this.FREQUENCYLENGTH; i++) {
+            frekvensListe[i] = frekvensTabell[i] & 0xFF;
         }
 
         HuffmanNode rootNode = generateHuffmanTree(frekvensListe);
-        
+
         byte[] fil = new byte[input.length - this.FREQUENCYLENGTH];
         System.arraycopy(input, this.FREQUENCYLENGTH, fil, 0, fil.length);
 
-        List<Boolean> bits = convertBytesToBits(fil);
+        BitSetCollection bits = convertBytesToBits(fil);
         HuffmanNode node = rootNode;
         try (ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream()) {
-            for(Boolean bool : bits) {
-                if (bool) {
-                    node = node.hNode;
-                } else {
-                    node = node.vNode;
-                }
-    
+            for (int i = 0; i < bits.bitLength; i++) {
+                node = bits.bitSet.get(i) ? node.hNode : node.vNode;
+
                 if (node.hNode == null && node.vNode == null) {
                     byteArrayOutputStream.write(node.tegn);
                     node = rootNode;
@@ -164,35 +115,6 @@ class Huffman {
         }
     }
 
-    private byte[] convertBitsToBytes(List<Boolean> bits) {
-        int byteLengde = (bits.size() + 7) / 8;
-        byte[] byteListe = new byte[byteLengde + 1];
-
-        for (int i = 0; i < bits.size(); i++) {
-            if(bits.get(i)) {
-                byteListe[i/8] |= (1 << (7 - (i % 8)));
-            }
-        }
-
-        //Legger på lengden på de gyldige bitsene i byteListe 
-        byteListe[byteLengde] = (byte) bits.size();
-        return byteListe;
-    }
-    
-    private List<Boolean> convertBytesToBits(byte[] byteListe) {
-        int gydligeBits = (int) byteListe[byteListe.length - 1];
-        List<Boolean> innpakketKode = new ArrayList<>();
-
-        for (byte b : byteListe) {
-            for (int i = 7; i >= 0; i--) {
-                boolean bit = (b & (1 << i)) != 0;
-                innpakketKode.add(bit);
-            }
-        }
-
-        return new ArrayList<Boolean>(innpakketKode.subList(0, gydligeBits));
-    }
-
     public int[] generateFrequencyList(byte[] input) {
         int[] frekvensListe = new int[this.FREQUENCYLENGTH];
         Arrays.setAll(frekvensListe, i -> 0);
@@ -201,6 +123,73 @@ class Huffman {
             frekvensListe[b & 0xFF] += 1;
         }
         return frekvensListe;
+    }
+
+    public HuffmanNode generateHuffmanTree(int[] frekvensListe) {
+        PriorityQueue<HuffmanNode> nQueue = new PriorityQueue<>();
+
+        for (int i = 0; i < frekvensListe.length; i++) {
+            int element = frekvensListe[i];
+            if (element > 0) {
+                nQueue.add(new HuffmanNode((byte) i, element));
+            }
+        }
+
+        return generateRootNodeFromMinHeap(nQueue);
+    }
+
+    public HuffmanNode generateRootNodeFromMinHeap(PriorityQueue<HuffmanNode> nQueue) {
+        while (nQueue.size() > 1) {
+            HuffmanNode a = nQueue.poll();
+            HuffmanNode b = nQueue.poll();
+
+            HuffmanNode combo = new HuffmanNode(a, b);
+            nQueue.add(combo);
+        }
+        return nQueue.poll();
+    }
+
+    public Map<Byte, BitSetCollection> generateCodes(HuffmanNode rootNode) {
+        if (rootNode == null || rootNode.getClass() != HuffmanNode.class) {
+            throw new IllegalArgumentException(rootNode + " er enten null eller ikke en HuffmanNode");
+        }
+        Map<Byte, BitSetCollection> codeMap = new HashMap<>();
+
+        exploreHuffmanNode(rootNode, new BitSet(), 0, codeMap);
+
+        return codeMap;
+    }
+
+    private void exploreHuffmanNode(HuffmanNode node, BitSet bits, int bitIndex, Map<Byte, BitSetCollection> codeMap) {
+        if (node.vNode == null & node.hNode == null) {
+            codeMap.put(node.tegn, new BitSetCollection((BitSet) bits.clone(), bitIndex));
+            return;
+        }
+
+        bits.clear(bitIndex);
+        exploreHuffmanNode(node.vNode, bits, bitIndex + 1, codeMap);
+
+        bits.set(bitIndex);
+        exploreHuffmanNode(node.hNode, bits, bitIndex + 1, codeMap);
+    }
+
+    private byte[] convertBitsToBytes(BitSetCollection bits) {
+        byte[] byteListe = bits.bitSet.toByteArray();
+        byte[] numberOfBits = ByteBuffer.allocate(Long.BYTES).putLong(bits.bitLength).array();
+
+        byte[] result = new byte[(int) (bits.bitLength + 7) / 8 + Long.BYTES];
+        System.arraycopy(byteListe, 0, result, 0, byteListe.length);
+        System.arraycopy(numberOfBits, 0, result, byteListe.length, Long.BYTES);
+        return result;
+    }
+
+    private BitSetCollection convertBytesToBits(byte[] byteListe) {
+        byte[] gydligeBitsArray = new byte[Long.BYTES];
+        System.arraycopy(byteListe, byteListe.length - Long.BYTES, gydligeBitsArray, 0, Long.BYTES);
+        long gyldigeBits = ByteBuffer.wrap(gydligeBitsArray).getLong();
+
+        BitSet innpakketKode = BitSet.valueOf(Arrays.copyOf(byteListe, byteListe.length - 1));
+        return new BitSetCollection(innpakketKode.get(0, (int) gyldigeBits), gyldigeBits);
     }
 
     public byte[] encodeStringToBytes(String input) {
@@ -220,7 +209,6 @@ class Huffman {
     }
 }
 
-
 class HuffmanNode implements Comparable<HuffmanNode> {
     final Byte tegn;
     int frekvens;
@@ -234,7 +222,7 @@ class HuffmanNode implements Comparable<HuffmanNode> {
         this.hNode = null;
     }
 
-    public HuffmanNode (HuffmanNode vNode, HuffmanNode hNode) {
+    public HuffmanNode(HuffmanNode vNode, HuffmanNode hNode) {
         this.tegn = null;
         this.frekvens = vNode.frekvens + hNode.frekvens;
         this.vNode = vNode;
@@ -243,14 +231,26 @@ class HuffmanNode implements Comparable<HuffmanNode> {
 
     @Override
     public boolean equals(Object obj) {
-        if (this == obj) return true;
-        if (obj == null || getClass() != obj.getClass()) return false;
+        if (this == obj)
+            return true;
+        if (obj == null || getClass() != obj.getClass())
+            return false;
         HuffmanNode node = (HuffmanNode) obj;
         return frekvens == node.frekvens;
-   }
+    }
 
-   @Override
+    @Override
     public int compareTo(HuffmanNode other) {
         return Integer.compare(this.frekvens, other.frekvens);
+    }
+}
+
+class BitSetCollection {
+    public BitSet bitSet;
+    public long bitLength;
+
+    public BitSetCollection(BitSet bitSet, long bitLength) {
+        this.bitSet = bitSet;
+        this.bitLength = bitLength;
     }
 }
