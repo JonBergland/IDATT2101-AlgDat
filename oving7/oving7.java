6acfb52586af0372f3e64b81433dcfb4759e48b3
Jon Bergland <144770553+JonBergland@users.noreply.github.com>
2024-11-16 12:58:18 +0100

    feat: update Djikstra with new method

--- oving7/oving7.java
+++ oving7/oving7.java
@@ -1,3 +1,5 @@
+package oving7;
+
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
@@ -23,7 +25,7 @@ class Run {
         Node node6 = new Node(6);
         Node node7 = new Node(7);
 
-        graf.addKant(new Kant(node7, node2, 6));
+        graf.addKant(new Kant(node7, node3, 6));
         graf.addKant(new Kant(node1, node2, 4));
         graf.addKant(new Kant(node1, node4, 2));
         graf.addKant(new Kant(node2, node3, 2));
@@ -72,13 +74,56 @@ class Djikstra {
                 int nyAvstand = k.vekt + neste.avstand;
                 if (til.avstand == Integer.MAX_VALUE) {
                     til.avstand = nyAvstand;
+                    til.setKortesteKant(k);
+                    nQueue.add(til);
+                } else if (nyAvstand < til.avstand) {
+                    til.avstand = nyAvstand;
+                    til.setKortesteKant(k);
+                }
+            }
+        }
+        return graf;
+    }
+
+    public Graf kortesteVei(Graf graf, Node startNode, Node maalNode) {
+        // Snur grafen for å ta utgangspunkt i målnoden
+        Graf omvGraf = graf.snuGraf();
+        Stack<Node> grStack = omvGraf.getStack();
+        if (!grStack.contains(startNode) || !grStack.contains(maalNode)) {
+            throw new IllegalArgumentException("Noden er ikke i Grafen");
+        }
+
+        maalNode.setAvstand(0);
+
+        PriorityQueue<Node> nQueue = new PriorityQueue<>();
+
+        // Bruker kantene ut fra mål noden til å sette avstanden i nabonodene
+        for (Kant k : maalNode.kantListe) {
+            Node til = k.til;
+            til.avstand = k.vekt;
+            nQueue.add(til);
+        }
+
+        // Gjentar dette så lenge det finnes nye nabonoder å gå til eller 
+        // Nabo noden blir tatt ut av 
+        while (nQueue.size() > 0) {
+            Node neste = nQueue.poll();
+            if (neste.equals(startNode)) {
+                return omvGraf;
+            }
+            for (Kant k : neste.kantListe) {
+                Node til = k.til;
+                int nyAvstand = k.vekt + neste.avstand;
+                if (til.avstand == Integer.MAX_VALUE) {
+                    til.avstand = nyAvstand;
                     nQueue.add(til);
                 } else if (nyAvstand < til.avstand) {
                     til.avstand = nyAvstand;
                 }
             }
         }
-        return graf;
+
+        return omvGraf;
     }
 }
 
@@ -86,14 +131,23 @@ class Node implements Comparable<Node> {
     public int nr;
     public int avstand;
     public List<Kant> kantListe = new ArrayList<>();
+    public Kant kortesteKant;
 
     public Node(int nr) {
         this.nr = nr;
         this.avstand = Integer.MAX_VALUE;
+        this.kortesteKant = null;
     }
 
     public void setAvstand(int nyAvstand) {
         this.avstand = nyAvstand;
+    }
+
+    public void setKortesteKant(Kant kant) {
+        if (kant == null || kant.getClass() != Kant.class) {
+            throw new IllegalArgumentException("Kanten er ikke riktig objekt eller null");
+        }
+        this.kortesteKant = kant;
     }
 
     @Override
